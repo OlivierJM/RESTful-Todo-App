119c071ae8026560a6a78cdde3421eee8e3693ea
OlivierJM <manolivier93@gmail.com>
2021-01-27 22:13:46 +0200

    fix the return and add helpful comments

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,54 +1,53 @@
+import React, { Fragment, useEffect, useState } from 'react';
 
-import React, {Fragment, useEffect, useState} from "react";
-const ListTodos =()=>{
-    const [todos, setTodos]= useState([]);
-    const getTodos = async() =>{
-       try {
-            const response =  await fetch("http://localhost:3001/todo");
-            const results = await response.json();
-            setTodos(results);
-            console.log(results);
-       } catch (error) {
-           console.error(`message : ${error.message}`);
-       }};
-   
+// prefer to use named functions for components than allow functions, they are easier to debug
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+  const getTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/todo');
+      const results = await response.json();
+      setTodos(results);
+      console.log(results);
+    } catch (error) {
+      console.error(`message : ${error.message}`);
+    }
+  };
 
-    useEffect(()=>{
-        getTodos();
-    },[]); 
-   
-    console.log(todos);
+  useEffect(() => {
+    getTodos();
+  }, []);
 
-    const ListData = todos.forEach( todo => 
-            
-            <tr>
-                 <td key={todo.id}>{todo.discription}</td>
-                 <td>Edit</td>
-                 <td>Delete</td>
-            </tr>
-         );
+  console.log(todos);
 
-    return(
-        <Fragment>
-            
-    <h2>Todo List</h2>
+  // use camelCase for variables whenever you can
+  // forEach does not return anything, it just loops through an array
+  // when using => you either immediately return using these ()
+  // or explicitly return with the keyword
+  const ListData = todos.map((todo) => (
+    // key needs to be put at the root of every returned element not the child
+    <tr key={todo.id}>
+      <td>{todo.description}</td>
+      <td>Edit</td>
+      <td>Delete</td>
+    </tr>
+  ));
 
-    <table className="table mt-5 text-center">
-                
-      <thead>
-        <tr>
-          <th>Description</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-      {ListData}
-      </tbody>
-     
-  </table>
-        </Fragment>
+  return (
+    <Fragment>
+      <h2>Todo List</h2>
 
-    )
-};  
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>{ListData}</tbody>
+      </table>
+    </Fragment>
+  );
+};
 export default ListTodos;
